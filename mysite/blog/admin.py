d25c6e6cdf82994f0d41e07ebe59406f8527ecd7
alex-njuguna <njugunakinuthia013@gmail.com>
2023-08-16 02:22:11 +0300

    admin site customization for the post model

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+# admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """customize what is displayed on the admin page
+    by the Post model"""
+    list_display = ['title', 'slug', 'author', 
+                    'publish', 'status']
+    list_filter = ['status', 'created', 'publish', 
+                   'author']
+    search_fields = ['title', 'body']
+
+    prepopulated_fields = {
+        'slug': ['title']
+    }
+    raw_id_fields = ['author']
+
+    date_hierarchy = 'publish'
+
+    ordering = ['status', 'publish']
+
+
+
+
