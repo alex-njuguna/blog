bc59c7fe3a2bac792a12691a3c09648b43342db4
alex-njuguna <njugunakinuthia013@gmail.com>
2023-08-16 02:58:57 +0300

    dislay all published posts views

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Post
+
+
+def post_list(request):
+    posts = Post.published.all()
+
+    context = {
+        'posts': posts
+    }
+    
+    return render(request, 'blog/post/list.html', context)
+
+
+
