ffb86c8f55742855034e9c4c34f85a892e572ef8
alex-njuguna <njugunakinuthia013@gmail.com>
2023-08-16 22:04:20 +0300

    pagination functionality in post list

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Post
 
 
 def post_list(request):
     """display all published posts"""
-    posts = Post.published.all()
+    object_list = Post.published.all()
+
+    paginator = Paginator(object_list, 3) # 3 posts in each page
+
+    page = request.GET.get('page')
+
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # if page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # if page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
 
     context = {
+        'page': page,
         'posts': posts
     }
 
