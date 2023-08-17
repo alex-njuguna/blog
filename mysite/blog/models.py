d221e6d0758929287ae7eb856b69fa890f578590
alex-njuguna <njugunakinuthia013@gmail.com>
2023-08-17 10:16:26 +0300

    comments model

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -50,6 +50,23 @@ class Post(models.Model):
         return self.title
 
 
+class Comment(models.Model):
+    # model for comments
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post.title}'
+
 
 
 
