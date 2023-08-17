79eb1ca6a9e04c55230b4e8fa23d1c9d852728bc
alex-njuguna <njugunakinuthia013@gmail.com>
2023-08-17 10:25:16 +0300

    models form for the comments

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
+from .models import Comment
+
 
 class EmailPostForm(forms.Form):
     # form to email a post
@@ -8,3 +10,12 @@ class EmailPostForm(forms.Form):
     to = forms.EmailField()
     comments = forms.CharField(required=False, 
                                widget=forms.Textarea)
+    
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ['name', 'email', 'body']
+
+
