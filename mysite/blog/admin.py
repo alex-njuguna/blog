9328f015bd62795d4fda8691ae371721048053df
alex-njuguna <njugunakinuthia013@gmail.com>
2023-08-17 10:20:48 +0300

    customize Comment model admin

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post, Comment
 
 
 # admin.site.register(Post)
@@ -23,6 +23,11 @@ class PostAdmin(admin.ModelAdmin):
 
     ordering = ['status', 'publish']
 
-
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    # admin view of Comment model
+    list_display = ['name', 'email', 'post', 'created', 'active']
+    list_filter = ['active', 'created', 'updated']
+    search_fields = ['name', 'email', 'body']
 
 
