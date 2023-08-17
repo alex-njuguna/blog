5b969c7af8650366caaf4b65555f217b1bd69a58
alex-njuguna <njugunakinuthia013@gmail.com>
2023-08-17 08:26:34 +0300

    class based view for list view

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
+from django.views.generic import ListView
+
 from .models import Post
 
 
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+
+    
+"""
 def post_list(request):
-    """display all published posts"""
+   # display all published post
     object_list = Post.published.all()
 
     paginator = Paginator(object_list, 3) # 3 posts in each page
@@ -27,7 +38,7 @@ def post_list(request):
     }
 
     return render(request, 'blog/post/list.html', context)
-
+"""
 
 def post_detail(request, year, month, day, post):
     """show a specific post"""
